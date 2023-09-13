2526b34e55583213161955d3be76ba9f12c8fc08
C1330938 Aron Ribeiro Chaves <c1330938@interno.bb.com.br>
2023-09-13 01:10:42 -0300

    adding GiftResponse file

---
+++ GiftResponse.js
@@ -0,0 +1,256 @@
+// GiftResponse.js
+
+// Handling functions for TikTok gifts
+function handleTennis() {
+    console.log("Gift: Tennis received");
+}
+
+function handleLoveLetter() {
+    console.log("Gift: Love letter received");
+}
+
+function handleFootball() {
+    console.log("Gift: Football received");
+}
+
+function handleGG() {
+    console.log("Gift: GG received");
+}
+
+function handleHiJuly() {
+    console.log("Gift: Hi July received");
+}
+
+function handleMiniSpeaker() {
+    console.log("Gift: Mini speaker received");
+}
+
+function handleLightningBolt() {
+    console.log("Gift: Lightning bolt received");
+}
+
+function handleCoffee() {
+    console.log("Gift: Coffee received");
+}
+
+function handleIceCreamCone() {
+    console.log("Gift: Ice cream cone received");
+}
+
+function handleWeights() {
+    console.log("Gift: Weights received");
+}
+
+function handleRose() {
+    console.log("Gift: Rose received");
+}
+
+function handleTikTokLogo() {
+    console.log("Gift: TikTok logo received");
+}
+
+function handleAlienPeaceSign() {
+    console.log("Gift: Alien peace sign received");
+}
+
+function handleGardenGnome() {
+    console.log("Gift: Garden gnome received");
+}
+
+function handleSquirrel() {
+    console.log("Gift: Squirrel received");
+}
+
+function handleDaisies() {
+    console.log("Gift: Daisies received");
+}
+
+function handlePinkShoes() {
+    console.log("Gift: Pink shoes received");
+}
+
+function handleDuckling() {
+    console.log("Gift: Duckling received");
+}
+
+function handleChic() {
+    console.log("Gift: Chic received");
+}
+
+function handlePandas() {
+    console.log("Gift: Pandas received");
+}
+
+function handleFingerHeart() {
+    console.log("Gift: Finger heart received");
+}
+
+function handleMic() {
+    console.log("Gift: Mic received");
+}
+
+function handleHi() {
+    console.log("Gift: Hi received");
+}
+
+function handleCottonsShell() {
+    console.log("Gift: Cotton’s shell received");
+}
+
+function handleBravo() {
+    console.log("Gift: Bravo received");
+}
+
+function handleDanceTogether() {
+    console.log("Gift: Dance Together received");
+}
+
+function handleGoose() {
+    console.log("Gift: Goose received");
+}
+
+function handleLoveBalloon() {
+    console.log("Gift: LOVE Balloon received");
+}
+
+function handleSwan() {
+    console.log("Gift: Swan received");
+}
+
+function handleTikTokTrophy() {
+    console.log("Gift: TikTok Trophy received");
+}
+
+function handleShoes() {
+    console.log("Gift: Shoes received");
+}
+
+function handleSilverGummy() {
+    console.log("Gift: Silver Gummy received");
+}
+
+function handlePearl() {
+    console.log("Gift: Pearl received");
+}
+
+function handleTrain() {
+    console.log("Gift: Train received");
+}
+
+function handleBadminton() {
+    console.log("Gift: Badminton received");
+}
+
+function handleTravelWithYou() {
+    console.log("Gift: Travel With You received");
+}
+
+function handleDinosaur() {
+    console.log("Gift: Dinosaur received");
+}
+
+function handleDiscoBall() {
+    console.log("Gift: Disco Ball received");
+}
+
+function handleEmailMessage() {
+    console.log("Gift: Email Message received");
+}
+
+function handleGalaxy() {
+    console.log("Gift: Galaxy received");
+}
+
+function handleGerryTheGiraffe() {
+    console.log("Gift: Gerry the Giraffe received");
+}
+
+function handleGoldGummy() {
+    console.log("Gift: Gold Gummy received");
+}
+
+function handleGoldMine() {
+    console.log("Gift: Gold Mine received");
+}
+
+function handleMagicLamp() {
+    console.log("Gift: Magic Lamp received");
+}
+
+function handleMirrorFlower() {
+    console.log("Gift: Mirror Flower received");
+}
+
+function handleSpace() {
+    console.log("Gift: Space received");
+}
+
+function handleWatermelonLove() {
+    console.log("Gift: Watermelon Love received");
+}
+
+function handleDiamondTree() {
+    console.log("Gift: Diamond Tree received");
+}
+
+function handleFireworks() {
+    console.log("Gift: Fireworks received");
+}
+
+function handleDiamond() {
+    console.log("Gift: Diamond received");
+}
+
+// Export all handling functions
+module.exports = {
+    handleTennis,
+    handleLoveLetter,
+    handleFootball,
+    handleGG,
+    handleHiJuly,
+    handleMiniSpeaker,
+    handleLightningBolt,
+    handleCoffee,
+    handleIceCreamCone,
+    handleWeights,
+    handleRose,
+    handleTikTokLogo,
+    handleAlienPeaceSign,
+    handleGardenGnome,
+    handleSquirrel,
+    handleDaisies,
+    handlePinkShoes,
+    handleDuckling,
+    handleChic,
+    handlePandas,
+    handleFingerHeart,
+    handleMic,
+    handleHi,
+    handleCottonsShell,
+    handleBravo,
+    handleDanceTogether,
+    handleGoose,
+    handleLoveBalloon,
+    handleSwan,
+    handleTikTokTrophy,
+    handleShoes,
+    handleSilverGummy,
+    handlePearl,
+    handleTrain,
+    handleBadminton,
+    handleTravelWithYou,
+    handleDinosaur,
+    handleDiscoBall,
+    handleEmailMessage,
+    handleGalaxy,
+    handleGerryTheGiraffe,
+    handleGoldGummy,
+    handleGoldMine,
+    handleMagicLamp,
+    handleMirrorFlower,
+    handleSpace,
+    handleWatermelonLove,
+    handleDiamondTree,
+    handleFireworks,
+    handleDiamond,
+};
