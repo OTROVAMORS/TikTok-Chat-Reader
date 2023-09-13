4684842eb7b9432c772e2512bc792bc2b5873feb
C1330938 Aron Ribeiro Chaves <c1330938@interno.bb.com.br>
2023-09-13 01:11:12 -0300

    adding app file

---
+++ public/app.js
@@ -0,0 +1,114 @@
+// This will use the demo backend if you open index.html locally via file://, otherwise your server will be used
+let backendUrl = location.protocol === 'file:' ? "https://tiktok-chat-reader.zerody.one/" : undefined;
+let connection = new TikTokIOConnection(backendUrl);
+
+// Counter for gifts
+let diamondsCount = 0;
+
+// These settings are defined by obs.html
+if (!window.settings) window.settings = {};
+
+$(document).ready(() => {
+    $('#connectButton').click(connect);
+    $('#uniqueIdInput').on('keyup', function (e) {
+        if (e.key === 'Enter') {
+            connect();
+        }
+    });
+
+    if (window.settings.username) connect();
+})
+
+function connect() {
+    let uniqueId = window.settings.username || $('#uniqueIdInput').val();
+    if (uniqueId !== '') {
+
+        $('#stateText').text('Connecting...');
+
+        connection.connect(uniqueId, {
+            enableExtendedGiftInfo: true
+        }).then(state => {
+            $('#stateText').text(`Connected to roomId ${state.roomId}`);
+
+            // reset gifts counter
+            diamondsCount = 0;
+
+        }).catch(errorMessage => {
+            $('#stateText').text(errorMessage);
+
+            // schedule next try if obs username set
+            if (window.settings.username) {
+                setTimeout(() => {
+                    connect(window.settings.username);
+                }, 30000);
+            }
+        })
+
+    } else {
+        alert('No username entered');
+    }
+}
+
+// Add a new gift to the gift container
+function addGiftItem(data) {
+    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.giftcontainer');
+
+    if (container.find('div').length > 200) {
+        container.find('div').slice(0, 100).remove();
+    }
+
+    let streakId = data.userId.toString() + '_' + data.giftId;
+
+    let html = `
+        <div data-streakid=${isPendingStreak(data) ? streakId : ''}>
+            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
+            <span>
+                <b>${generateUsernameLink(data)}:</b> <span>${data.describe}</span><br>
+                <div>
+                    <table>
+                        <tr>
+                            <td><img class="gifticon" src="${data.giftPictureUrl}"></td>
+                            <td>
+                                <span>Name: <b>${data.giftName}</b> (ID:${data.giftId})<span><br>
+                                <span>Repeat: <b style="${isPendingStreak(data) ? 'color:red' : ''}">x${data.repeatCount.toLocaleString()}</b><span><br>
+                                <span>Cost: <b>${(data.diamondCount * data.repeatCount).toLocaleString()} Diamonds</b><span>
+                            </td>
+                        </tr>
+                    </tabl>
+                </div>
+            </span>
+        </div>
+    `;
+
+    let existingStreakItem = container.find(`[data-streakid='${streakId}']`);
+
+    if (existingStreakItem.length) {
+        existingStreakItem.replaceWith(html);
+    } else {
+        container.append(html);
+    }
+
+    container.stop();
+    container.animate({
+        scrollTop: container[0].scrollHeight
+    }, 800);
+}
+
+// Gift event handler
+connection.on('gift', (data) => {
+    if (!isPendingStreak(data) && data.diamondCount > 0) {
+        diamondsCount += (data.diamondCount * data.repeatCount);
+        updateRoomStats();
+    }
+
+    // Show only gift events
+    addGiftItem(data);
+});
+
+// ... Other event handlers ...
+
+function updateRoomStats() {
+    $('#roomStats').html(`Earned Diamonds: <b>${diamondsCount.toLocaleString()}</b>`)
+}
+
+// ... Other functions and event handlers ...
